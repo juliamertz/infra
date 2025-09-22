23e9bcbf2d8412960885f7363f1d01735cb1b8ed
Julia Mertz <info@juliamertz.dev>
2025-09-22 10:05:32 +0200

    wip

--- terraform/modules/hcloud/talos_cluster/talos_patch_control_plane.tf
+++ terraform/modules/hcloud/talos_cluster/talos_patch_control_plane.tf
@@ -110,7 +110,7 @@ locals {
         registries = var.registries
       }
       cluster = {
-        allowSchedulingOnControlPlanes = var.control_plane_allow_schedule || var.worker_count <= 0
+        allowSchedulingOnControlPlanes = var.control_plane_allow_schedule || var.worker_count + length(var.worker_nodes) <= 0
         network = {
           dnsDomain = var.cluster_domain
           podSubnets = [
