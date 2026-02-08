f06961230685c5a3cafb49c42cbaf0e83f76d648
Julia Mertz <info@juliamertz.dev>
2026-02-08 02:05:39 +0100

    feat: fetch grafana dashboard from external url

--- kubernetes/manifest/apps/grafana/default.nix
+++ kubernetes/manifest/apps/grafana/default.nix
@@ -1,6 +1,8 @@
 {
   config,
   kubenix,
+  pkgs,
+  lib,
   crds,
   ...
 }: {
@@ -19,20 +21,26 @@
       "pod-security.kubernetes.io/warn" = "privileged";
     };
 
-    resources.configMaps.nettenshop-dashboard = {
-      metadata.labels.grafana_dashboard = "1";
-      data."nettenshop.json" = builtins.readFile ./dashboards/nettenshop.json;
-    };
-
-    resources.configMaps.headscale-dashboard = {
-      metadata.labels.grafana_dashboard = "1";
-      data."headscale.json" = builtins.readFile ./dashboards/headscale.json;
-    };
-
-    resources.configMaps.cloudnative-pg-dashboard = {
-      metadata.labels.grafana_dashboard = "1";
-      data."cloudnative-pg.json" = builtins.readFile ./dashboards/cloudnative-pg.json;
-    };
+    resources.configMaps = let
+      dashboards = {
+        nettenshop = builtins.readFile ./dashboards/nettenshop.json;
+        headscale = builtins.readFile (pkgs.fetchurl {
+          url = "https://grafana.com/api/dashboards/24516/revisions/3/download";
+          sha256 = "sha256-zDJU2fxVnM70O8UevoHSVb/egquMFG52JJJQXDa2LCs=";
+        });
+        cloudnative-pg = builtins.readFile (pkgs.fetchurl {
+          url = "https://grafana.com/api/dashboards/20417/revisions/4/download";
+          sha256 = "sha256-+YIJCenupF1TfvIvJwOVMILSZ6wdD1MPIS1E1ylgD6o=";
+        });
+      };
+      configMapNames = lib.attrNames dashboards |> map (v: "${v}-dashboard");
+    in
+      lib.genAttrs configMapNames (configMapName: let
+        name = lib.removeSuffix "-dashboard" configMapName;
+      in {
+        metadata.labels.grafana_dashboard = "1";
+        data."${name}.json" = dashboards.${name};
+      });
 
     resources.helmRepositories.grafana.spec = {
       interval = "30m";
