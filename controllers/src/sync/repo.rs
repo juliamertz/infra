e00639c80bb257728486938fb7cdf4a7b339f676
Julia Mertz <info@juliamertz.dev>
2026-02-10 22:41:05 +0100

    fix(sync): persist updated git index

--- controllers/src/sync/repo.rs
+++ controllers/src/sync/repo.rs
@@ -114,6 +114,10 @@ impl Repo {
         )
         .unwrap();
 
+        let index_path = self.inner.git_dir().join("index");
+        let mut index_file = gix::index::File::from_state(index, index_path);
+        index_file.write(Default::default()).unwrap();
+
         info!("checked out {oid}");
         Ok(())
     }
