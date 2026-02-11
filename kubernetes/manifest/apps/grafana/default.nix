3fdca7b17d20ce633b5b50aa1d683da6d5b7aceb
Julia Mertz <info@juliamertz.dev>
2026-02-11 16:53:50 +0100

    fix: bring back loki (missed during migration)
    fix

--- kubernetes/manifest/apps/grafana/default.nix
+++ kubernetes/manifest/apps/grafana/default.nix
@@ -71,19 +71,11 @@
               cpu = "50m";
               memory = "256Mi";
             };
-            limits = {
-              cpu = "250m";
-              memory = "512Mi";
-            };
           };
           sidecar.resources = {
             requests = {
               cpu = "25m";
               memory = "128Mi";
-            };
-            limits = {
-              cpu = "100m";
-              memory = "256Mi";
             };
           };
           admin = {
@@ -222,6 +214,70 @@
               }
             ];
           };
+        };
+      };
+    };
+
+    resources.helmReleases.loki-stack.spec = {
+      interval = "5m";
+      chart.spec = {
+        chart = "loki-stack";
+        version = "2.10.2";
+        sourceRef = {
+          kind = "HelmRepository";
+          name = "grafana";
+        };
+        interval = "1m";
+      };
+      values = {
+        loki = {
+          enabled = true;
+          persistence = {
+            enabled = true;
+            size = "5Gi";
+            storageClassName = "longhorn-local";
+          };
+          config = {
+            auth_enabled = false;
+            limits_config.retention_period = "168h";
+          };
+          resources = {
+            requests = {
+              cpu = "15m";
+              memory = "192Mi";
+            };
+            limits = {
+              cpu = "100m";
+              memory = "512Mi";
+            };
+          };
+        };
+        grafana.sidecar.datasources.enabled = false;
+        promtail = {
+          resources = {
+            requests = {
+              cpu = "50m";
+              memory = "128Mi";
+            };
+            limits = {
+              cpu = "200m";
+              memory = "256Mi";
+            };
+          };
+          tolerations = [
+            {
+              key = "node.kubernetes.io/role";
+              operator = "Equal";
+              value = "gameserver-node";
+              effect = "NoExecute";
+            }
+            {
+              key = "node.kubernetes.io/role";
+              operator = "Equal";
+              value = "autoscaler-node";
+              effect = "NoExecute";
+            }
+          ];
         };
       };
     };
