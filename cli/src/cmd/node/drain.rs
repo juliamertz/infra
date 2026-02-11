4ebbd2c5e04966f66374d1b04f74cd606047eacd
Julia Mertz <info@juliamertz.dev>
2026-02-11 21:54:27 +0100

    fix: proxy kubectl drain output to tracing

--- cli/src/cmd/node/drain.rs
+++ cli/src/cmd/node/drain.rs
@@ -1,22 +1,42 @@
+use std::process::Stdio;
+
 use anyhow::Result;
+use tokio::io::{AsyncBufReadExt, AsyncRead, BufReader};
 use tokio::process::Command;
+use tracing::debug;
 
 use crate::Context;
 use crate::talos::Member;
 
+fn proxy_stdio<R>(reader: R)
+where
+    R: AsyncRead + Unpin + Send + 'static,
+{
+    let mut lines = BufReader::new(reader).lines();
+
+    tokio::spawn(async move {
+        while let Ok(Some(line)) = lines.next_line().await {
+            debug!("{line}")
+        }
+    });
+}
+
 pub async fn handle(_ctx: Context, member: &Member) -> Result<()> {
-    // TODO: find some way to pipe stderr of stdout to tracing with nice filters/formatting
-    let output = Command::new("kubectl")
+    let mut child = Command::new("kubectl")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .arg("drain")
         .arg(member.hostname())
         .arg("--ignore-daemonsets")
         .arg("--delete-emptydir-data")
         .arg("--timeout=5m")
-        .spawn()?
-        .wait_with_output()
-        .await?;
+        .spawn()?;
 
-    if !output.status.success() {
+    proxy_stdio(child.stdout.take().unwrap());
+    proxy_stdio(child.stderr.take().unwrap());
+
+    let status = child.wait().await?;
+    if !status.success() {
         anyhow::bail!("failed to drain node");
     }
 
