4bd68c6a21b793e7e505c6f599e9ed8d27149feb
Julia Mertz <info@juliamertz.dev>
2026-02-14 00:32:55 +0100

    wip: persisted nix dir for sync controller

--- controllers/src/sync/nix.rs
+++ controllers/src/sync/nix.rs
@@ -27,12 +27,19 @@ pub type BuildResult<T> = core::result::Result<T, BuildError>;
 #[instrument(err(Debug))]
 pub async fn build(dir: &Path, attrpath: &str) -> BuildResult<PathBuf> {
     info!("starting nix build");
+    let nix_path = "/builder/nix";
     let mut root_cmd = Command::new("nix");
     let cmd = root_cmd
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .current_dir(dir)
         .env("NIX_CONFIG", "build-users-group = ")
+        .arg("--eval-store")
+        .arg("/builder/nix/store")
+        .arg("--store")
+        .arg(format!(
+            "local?store={nix_path}/store&state={nix_path}/state&log={nix_path}/log"
+        ))
         .arg("--extra-experimental-features")
         .arg("nix-command flakes pipe-operators")
         .arg("build")
@@ -63,10 +70,7 @@ struct ObjectList {
 }
 
 #[instrument(err(Debug))]
-pub async fn build_kubenix(
-    dir: &Path,
-    attrpath: &str,
-) -> BuildResult<Vec<DynamicObject>> {
+pub async fn build_kubenix(dir: &Path, attrpath: &str) -> BuildResult<Vec<DynamicObject>> {
     let out_path = build(dir, attrpath).await?;
     let content = fs::read_to_string(&out_path).await?;
     let templated = vals::eval(&dir, &content).await?;
